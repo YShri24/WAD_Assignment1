fbfacefc6efc47bc56395871f0d4d9016c5efa15
YShri24 <yadavshrivardhan4950@gmail.com>
2026-02-13 09:42:03 +0530

    Added all project files

---
+++ Assignment1B/script.js
@@ -0,0 +1,78 @@
+document.getElementById("regForm").addEventListener("submit", function(e) {
+    e.preventDefault();
+    // DOB validation (must be 18+)
+    let dobInput = document.getElementById("dob").value;
+    let dob = new Date(dobInput);
+    let today = new Date();
+
+    let age = today.getFullYear() - dob.getFullYear();
+    let monthDiff = today.getMonth() - dob.getMonth();
+
+    if (monthDiff < 0 || (monthDiff === 0 && today.getDate() < dob.getDate())) {
+    age--;
+    }
+
+    if (age < 18) {
+    alert("You must be at least 18 years old.");
+    return;
+    }
+
+    // Get hobbies
+    let hobbies = [];
+    document.querySelectorAll(".hobby:checked").forEach(h => {
+        hobbies.push(h.value);
+    });
+
+    // Other hobby
+    if (document.getElementById("otherChk").checked) {
+        let other = document.getElementById("otherText").value;
+        if (other.trim() !== "") {
+            hobbies.push(other);
+        }
+    }
+
+    // Get gender
+    let gender = document.querySelector("input[name='gender']:checked").value;
+
+    // Collect form data
+    let userData = {
+        name: document.getElementById("name").value,
+        mobile: document.getElementById("mobile").value,
+        email: document.getElementById("email").value,
+        password: document.getElementById("password").value,
+        dob: document.getElementById("dob").value,
+        address: document.getElementById("address").value,
+        city: document.getElementById("city").value,
+        gender: gender,
+        hobbies: hobbies
+    };
+
+    // Store in localStorage
+    localStorage.setItem("user", JSON.stringify(userData));
+
+    // AJAX POST simulation
+    let xhr = new XMLHttpRequest();
+    xhr.open("POST", "result.html", true);
+    xhr.setRequestHeader("Content-Type", "application/json");
+
+    xhr.onload = function() {
+        if (xhr.status === 200 || xhr.status === 0) {
+            window.location.href = "result.html";
+        }
+    };
+
+    xhr.send(JSON.stringify(userData));
+});
+
+
+// Show/Hide Password
+document.getElementById("showPass").addEventListener("change", function() {
+    let passField = document.getElementById("password");
+
+    if (this.checked) {
+        passField.type = "text";      // show password
+    } else {
+        passField.type = "password";  // hide password
+    }
+});
+
